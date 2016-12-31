5245272b62ab22b734656bfeb773f9cc6da043ef
radioActivePiyish <piyushlionhearts@gmail.com>
2016-12-31 22:12:00 +0530

    device pixel integration and small fixes

--- app/src/main/java/com/example/piyush/mapp/MainActivity.java
+++ app/src/main/java/com/example/piyush/mapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 import android.widget.Button;
+import android.widget.EditText;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
 
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;// important
 
@@ -17,20 +21,50 @@ public class MainActivity extends AppCompatActivity {
 
         //LAYOOUT
         RelativeLayout ml = new RelativeLayout(this);
-        ml.setBackground(Color.BLACK);
+        ml.setBackgroundColor(Color.DKGRAY);
 
         //BUTTON
         Button rb = new Button(this);
-        rb.setBackground(Color.DKGRAY);
+        rb.setBackgroundColor(Color.RED);
+        rb.setText(R.string.button_text);
 
-        //BUTTON DETAILS
+        //USERNAME INPUT
+        EditText u_name = new EditText(this);
+        // IDS
+        rb.setId(1);
+
+        u_name.setId(2);
+
+
+
+        //BUTTON POSITION
         RelativeLayout.LayoutParams b_details = new RelativeLayout.LayoutParams(
                 WRAP_CONTENT,WRAP_CONTENT);
 
         b_details.addRule(RelativeLayout.CENTER_HORIZONTAL);
         b_details.addRule(RelativeLayout.CENTER_VERTICAL);
 
-        ml.addView(rb);//ADD LAYOUT
+
+        //USERNAME POSITION
+        RelativeLayout.LayoutParams u_details = new RelativeLayout.LayoutParams(
+                WRAP_CONTENT,WRAP_CONTENT);
+
+        u_details.addRule(RelativeLayout.ABOVE, rb.getId());
+        u_details.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        u_details.setMargins(0,0,0,50);
+
+
+        Resources r = getResources();
+        int px =(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
+
+        u_name.setWidth(px);
+
+        //ADD TO THIS LAYOUT (THIS NOW  BECOMES THE CHILD )
+        ml.addView(rb,b_details);
+        ml.addView(u_name,u_details);
+
+
         setContentView(ml);//SET CONTENT TO THIS VIEW
 
 
