31d8d6d8fa27f49c12049ff2a6e3116bdd976cbc
radioActivePiyish <piyushlionhearts@gmail.com>
2017-01-01 20:41:52 +0530

    initial event handling rest on evevts branch

--- event_handling/app/src/main/java/com/example/piyush/grid_layout/MainActivity.java
+++ event_handling/app/src/main/java/com/example/piyush/grid_layout/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.piyush.grid_layout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.big_button);
+        button.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        TextView changetext = (TextView)findViewById(R.id.my_text);
+                        changetext.setText("BIG BUTTON");
+
+                    }
+                }
+
+        );
+
+
+
+
     }
 }
