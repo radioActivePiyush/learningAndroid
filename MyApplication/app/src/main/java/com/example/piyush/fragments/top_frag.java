4578e83fd6c88a7a6b075726644c6dd6bc37ee98
radioActivePiyush <piyushlionhearts@gmail.com>
2017-05-11 13:43:40 +0530

    created two fragments xmls

---
+++ MyApplication/app/src/main/java/com/example/piyush/fragments/top_frag.java
@@ -0,0 +1,19 @@
+package com.example.piyush.fragments;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android .view.LayoutInflater;
+import android .view.View;
+import android.view.ViewGroup;
+
+
+
+public class top_frag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.top_frag, container, false);
+        return view;
+    }
+}
